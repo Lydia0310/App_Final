cc23491a63acb6b6c3e3064bf85e30360af23c83
zhang junyan <silenceleaf@gmail.com>
2014-12-01 17:24:51 -0500

    Structure

--- src/main/java/org/yueli/userinterface/frame/FirstPanel.java
+++ src/main/java/org/yueli/userinterface/frame/FirstPanel.java
@@ -5,8 +5,16 @@
  */
 package org.yueli.userinterface.frame;
 
+import org.yueli.business.schedule.Schedule;
+import org.yueli.business.workqueue.ScheduleRequest;
+import org.yueli.userinterface.schedule.SchedulePanel;
+
 import javax.swing.*;
 import java.awt.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.List;
 
 /**
  *
@@ -14,13 +22,38 @@ import java.awt.*;
  */
 public class FirstPanel extends JPanel {
 
-    private final LoginPanel loginPanel;
-    private Graphics2D paint;
+    //private final LoginPanel loginPanel;
+    private final SchedulePanel loginPanel;
     private final String versionInfo = "Name: Yue Li          ver. 1.01";
+    private SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     public FirstPanel() {
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-        loginPanel = new LoginPanel();
+        //loginPanel = new LoginPanel();
+        List<Schedule> scheduleList = new LinkedList<>();
+        try {
+            Schedule schedule1 = new Schedule(timeFormat.parse("2014-12-01 01:00:00"), timeFormat.parse("2014-12-01 03:00:00"),
+                    "test", ScheduleRequest.ScheduleType.Device);
+            Schedule schedule2 = new Schedule(timeFormat.parse("2014-12-01 05:00:00"), timeFormat.parse("2014-12-01 07:00:00"),
+                    "test", ScheduleRequest.ScheduleType.Device);
+            Schedule schedule3 = new Schedule(timeFormat.parse("2014-12-02 12:00:00"), timeFormat.parse("2014-12-02 18:00:00"),
+                    "test", ScheduleRequest.ScheduleType.Device);
+            Schedule schedule4 = new Schedule(timeFormat.parse("2014-12-03 05:00:00"), timeFormat.parse("2014-12-03 07:00:00"),
+                    "test", ScheduleRequest.ScheduleType.Device);
+            Schedule schedule5 = new Schedule(timeFormat.parse("2014-12-04 05:00:00"), timeFormat.parse("2014-12-04 07:00:00"),
+                    "test", ScheduleRequest.ScheduleType.Device);
+            Schedule schedule6 = new Schedule(timeFormat.parse("2014-12-05 05:00:00"), timeFormat.parse("2014-12-06 07:00:00"),
+                    "test", ScheduleRequest.ScheduleType.Device);
+            scheduleList.add(schedule1);
+            scheduleList.add(schedule2);
+            scheduleList.add(schedule3);
+            scheduleList.add(schedule4);
+            scheduleList.add(schedule5);
+            scheduleList.add(schedule6);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        loginPanel = new SchedulePanel(scheduleList);
         JLabel welcomeLabel = new JLabel("Welcome!   Please login.");
         welcomeLabel.setFont(new Font("Arial", Font.PLAIN, 48));
         Box row1 = new Box(BoxLayout.LINE_AXIS);
--- src/main/java/org/yueli/userinterface/schedule/SchedulePanel.java
+++ src/main/java/org/yueli/userinterface/schedule/SchedulePanel.java
@@ -4,6 +4,7 @@ import org.yueli.business.schedule.Schedule;
 import org.yueli.userinterface.frame.MainFrame;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.geom.Line2D;
 import java.text.ParseException;
@@ -16,15 +17,16 @@ import java.util.List;
  */
 public class SchedulePanel extends JPanel {
     private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-    private SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
     private final long millisPerDay = 24 * 60 * 60 * 1000;
+    private final long secondsPerDay = 24 * 60 * 60;
     private JPanel panel;
-    private int panelWidth = 500;
+    private int panelWidth = 680;
     private int panelHeight = 400;
     private int rowHeight = 30;
-    private int firstColumnWidth = 50;
+    private int firstColumnWidth = 120;
     private Dimension panelSize = new Dimension(panelWidth, panelHeight);
-    private Dimension rowPanelSize = new Dimension(panelWidth, rowHeight);
+    private Dimension rowPanelSize = new Dimension(panelWidth - 8, rowHeight);
     private Map<String, ScheduleUnit> slicedSchedule;
 
     private class ScheduleUnit {
@@ -90,11 +92,15 @@ public class SchedulePanel extends JPanel {
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
             Graphics2D paint = (Graphics2D) g;
-            paint.setStroke(new BasicStroke(3));
+            paint.setStroke(new BasicStroke(10));
             paint.setColor(MainFrame.blue);
             for (ScheduleUnit.Pair pair : scheduleList) {
-                double startX = (panelWidth / millisPerDay ) * (pair.getBeginTime() % millisPerDay) + firstColumnWidth;
-                double endX = (panelWidth / millisPerDay ) * (pair.getEndTime() % millisPerDay) + firstColumnWidth;
+                String[] splitTime = timeFormat.format(new Date(pair.beginTime)).split(":");
+                int beginInSeconds = Integer.valueOf(splitTime[0]) * 60 * 60 + Integer.valueOf(splitTime[1]) * 60 + Integer.valueOf(splitTime[2]);
+                splitTime = timeFormat.format(new Date(pair.endTime)).split(":");
+                int endInSeconds = Integer.valueOf(splitTime[0]) * 60 * 60 + Integer.valueOf(splitTime[1]) * 60 + Integer.valueOf(splitTime[2]);
+                double startX = ((double)(panelWidth - firstColumnWidth) / secondsPerDay) * beginInSeconds + firstColumnWidth;
+                double endX = ((double)(panelWidth - firstColumnWidth) / secondsPerDay) * endInSeconds + firstColumnWidth;
                 Shape line = new Line2D.Double(startX, 10, endX, 10);
                 paint.draw(line);
             }
@@ -104,10 +110,12 @@ public class SchedulePanel extends JPanel {
     public SchedulePanel(List<Schedule> scheduleList) {
         slicedSchedule = new HashMap<>();
         sliceScheduleList(scheduleList);
+        setBorder(BorderFactory.createLineBorder(MainFrame.blue, 2));
         // sort collection
         Collections.sort(scheduleList);
         setLayout(new BorderLayout());
-        panel = new JPanel(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
         JScrollPane scrollPane = new JScrollPane(panel);
         this.add(scrollPane, BorderLayout.CENTER);
         setMaximumSize(panelSize);
@@ -123,17 +131,17 @@ public class SchedulePanel extends JPanel {
         head.setMaximumSize(rowPanelSize);
         head.setMinimumSize(rowPanelSize);
         head.add(Box.createHorizontalStrut(firstColumnWidth), BorderLayout.WEST);
-        head.add(new JLabel("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24"), BorderLayout.CENTER);
+        head.add(new JLabel("00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24"), BorderLayout.CENTER);
         panel.add(head);
-        Calendar current = Calendar.getInstance();
-        current.setTime(scheduleList.get(0).getBeginTime());
-        Calendar nextDay = Calendar.getInstance();
-        nextDay.setTime(current.getTime());
-        nextDay.add(Calendar.DAY_OF_YEAR, 1);
-        Calendar lastDay = Calendar.getInstance();
-        lastDay.setTime(scheduleList.get(scheduleList.size() - 1).getEndTime());
-        while (nextDay.compareTo(lastDay) < 0) {
-            String currentDate = dateFormat.format(current.getTime());
+        long currentTime = scheduleList.get(0).getBeginTime().getTime();
+        long endTime = 0;
+        for (Schedule schedule : scheduleList) {
+            // get the largest end time
+            if (schedule.getEndTime().getTime() > endTime)
+                endTime = schedule.getEndTime().getTime();
+        }
+        while (currentTime < endTime) {
+            String currentDate = dateFormat.format(new Date(currentTime));
             ScheduleUnit scheduleUnit = slicedSchedule.get(currentDate);
             RowPanel rowPanel;
             if (scheduleUnit != null) {
@@ -143,26 +151,26 @@ public class SchedulePanel extends JPanel {
             }
             rowPanel.add(new JLabel(currentDate));
             panel.add(rowPanel);
-            current.add(Calendar.DAY_OF_YEAR, 1);
-            nextDay.add(Calendar.DAY_OF_YEAR, 1);
+            currentTime += millisPerDay;
         }
     }
 
     private void sliceScheduleList (List<Schedule> scheduleList) {
         for (Schedule schedule : scheduleList) {
-            long begin = schedule.getEndTime().getTime();
-            long end = schedule.getBeginTime().getTime();
+            long begin = schedule.getBeginTime().getTime();
+            long end = schedule.getEndTime().getTime();
             if (end - begin > millisPerDay) {
                 // cross one day
-                while (begin + millisPerDay < end) {
+                while (end - millisPerDay > begin) {
                     String currentDay = dateFormat.format(new Date(begin));
                     ScheduleUnit day = slicedSchedule.get(currentDay);
                     if (day == null)
                         day = new ScheduleUnit();
                     try {
+                        long today24 = dateFormat.parse(dateFormat.format(new Date(begin)) + " 24:00").getTime();
                         long nextDay00 = dateFormat.parse(dateFormat.format(new Date(begin + millisPerDay)) + " 00:00").getTime();
-                        day.addSchedule(begin, nextDay00);
-                        slicedSchedule.put(dateFormat.format(new Date(begin)), day);
+                        day.addSchedule(begin, today24);
+                        slicedSchedule.put(currentDay, day);
                         begin = nextDay00;
                     } catch (ParseException e) {
                         e.printStackTrace();
@@ -173,7 +181,7 @@ public class SchedulePanel extends JPanel {
                 if (day == null)
                     day = new ScheduleUnit();
                 day.addSchedule(begin, end);
-                slicedSchedule.put(dateFormat.format(new Date(begin)), day);
+                slicedSchedule.put(currentDay, day);
             } else {
                 // schedule less than one day
                 String currentDay = dateFormat.format(new Date(begin));
@@ -181,7 +189,7 @@ public class SchedulePanel extends JPanel {
                 if (day == null)
                     day = new ScheduleUnit();
                 day.addSchedule(begin, end);
-                slicedSchedule.put(dateFormat.format(new Date(begin)), day);
+                slicedSchedule.put(currentDay, day);
             }
         }
     }
