48f392804fe0124d173b8a3ed883dc320ffe341c
silenceleaf <silenceleaf@gmail.com>
2014-12-04 20:42:32 -0500

    package name

--- src/main/java/org/yueli/userinterface/AppEntrance.java
+++ src/main/java/org/yueli/userinterface/AppEntrance.java
@@ -15,11 +15,6 @@ import org.yueli.userinterface.frame.MainFrame;
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-
 /**
  * Created by zjy on 11/9/14.
  */
